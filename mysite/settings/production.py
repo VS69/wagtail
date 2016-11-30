fe57a4d31cc35953058842e871cf7ece1d057120
Volodymyr Salitrynskyi <vladimir.salitrinsky@gmail.com>
2016-11-30 11:23:55 +0000

    Start project my blog on Wagtail CMS

---
+++ mysite/settings/production.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+try:
+    from .local import *
+except ImportError:
+    pass
